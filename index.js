621773a5fa0cd351680f898a216c328675389aec
cxcatesc <costin@axl.media>
2020-10-07 10:23:09 +0300

    modified headless browser mode

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ const login = async () => {
 
     const browser = await puppeteer.launch({
       headless: true,
-      args: ["--no-sandbox", 'disable-setuid-sandbox']
     });
 
     const page = await browser.newPage();
