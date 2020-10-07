d59f1cced360809024b5d8300d7c15ec92398235
cxcatesc <costin@axl.media>
2020-10-07 10:38:13 +0300

    No sandbox pupeteer

--- index.js
+++ index.js
@@ -51,6 +51,7 @@ const login = async () => {
 
     const browser = await puppeteer.launch({
       headless: true,
+      args: ['--no-sandbox']
     });
 
     const page = await browser.newPage();
